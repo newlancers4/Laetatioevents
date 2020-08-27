dc986569d57aa8003613e2829dba1845843d207b
Pradeep <pradeepkps19@gmail.com>
2020-08-27 17:45:17 +0530

    index page update

--- eventManaging/script.js
+++ eventManaging/script.js
@@ -1,17 +1,11 @@
 window.onload = timeout;
-const body=document.getElementById("body")
-function timeout() {    
-    
+const body = document.getElementById("body")
+function timeout() {
     window.setTimeout("redirect()", 9000)
 }
 
-
 function redirect() {
     var splash = document.querySelector(".splash-container");
-//    body.style.overflow="visible"
-  //  body.style.padding=10+"px"
-    //body.style.margin=8+"px"
-    //console.log(splash)
     splash.remove()
 }
 window.onscroll = function () { myFunction() };
@@ -25,4 +19,25 @@ function myFunction() {
     } else {
         menu.classList.remove("sticky");
     }
+}
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex - 1].style.display = "block";
 }
